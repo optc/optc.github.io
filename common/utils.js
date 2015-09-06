63d654b55e4f2b86d15ebd57e513f7be55bc365b
optc <optc@users.noreply.github.com>
2015-09-06 11:38:38 +0100

    oops

--- common/utils.js
+++ common/utils.js
@@ -41,7 +41,7 @@ utils.parseUnits = function(skipIncomplete) {
 /* * * * * Thumbnail control * * * * */
 
 utils.getThumbnailUrl = function(n) {
-    if (window.units[n-1].incomplete)
+    if (window.units && window.units[n-1].incomplete)
         return 'http://onepiece-treasurecruise.com/wp-content/themes/onepiece-treasurecruise/images/noimage.png';
     var id = ('0000' + n).slice(-4).replace(/(057[54])/,'0$1'); // missing aokiji image
     return 'http://onepiece-treasurecruise.com/wp-content/uploads/f' + id + '.png';
