4608be39b322bc2ab65f57b82ddc4dd330e9d07d
optc <optc@users.noreply.github.com>
2015-11-03 15:51:51 +0000

    oops

--- damage/js/directives.js
+++ damage/js/directives.js
@@ -657,7 +657,7 @@ filters.range = function() {
 filters.decorate = function() {
     return function(input) {
         if (!input) return 'None';
-        return input
+        return (input.constructor == Array ? input[0] : input[1])
             .replace(/\[?(STR|DEX|QCK|PSY|INT|TND)\]?/g,'<span class="badge $1">$1</span>')
             .replace(/\[RCV\]/g,'<span class="badge RCV">RCV</span>');
 
