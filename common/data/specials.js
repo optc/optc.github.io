0d2ea7ec93fec3fdd92ffb0e777784c00979943b
Zach <Zee_n1@yahoo.com>
2016-02-18 12:21:09 -0600

    testing
    test

--- common/data/specials.js
+++ common/data/specials.js
@@ -882,14 +882,11 @@ window.specials = {
 		atk: function(p) {
 			return p.unit.class.has("Shooter") || p.unit.class.has("Freedom") ? 1.75 : 1;
 		},
-		rcv: function(p) { return 0.1; },
+		rcv: function(p) { return 2; },
 		type: "class"
 	},
 	882: {
-		atk: function(p) {
-			return p.unit.class.has("Shooter") || p.unit.class.has("Freedom") ? 1.75 : 1;
-		},
-		rcv: function(p) { return 0.1; },
+		rcv: function(p) { return 0.5; },
 		type: "class"
 	},
 	883: {
