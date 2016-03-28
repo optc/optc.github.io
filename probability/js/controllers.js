5d5f5c22e55b97174af0beff8f2a59277abb6a55
Christoph Gebendorfer <gebendorfer@icewave.org>
2016-03-28 19:15:06 +0200

    probability calculator

---
+++ probability/js/controllers.js
@@ -0,0 +1,251 @@
+/* jshint loopfunc: true */
+
+(function() {
+
+var controllers = { };
+
+/************
+ * MainCtrl *
+ ************/
+
+controllers.MainCtrl = ['$scope', '$rootScope', '$state', '$stateParams', '$controller', '$timeout', '$window', 'SpecialProbability', 'SocketProbability', function($scope, $rootScope, $state, $stateParams, $controller, $timeout, $window, SpecialProbability, SocketProbability) { 
+
+    $rootScope.character = null;
+    $rootScope.options = { transient: false };
+	
+    $scope.skillups = null;
+    $scope.copies = null;
+	$scope.slots = null;
+	$scope.specialEvent = false;
+	
+	$scope.specialProbabilityProgress = 0;
+	$scope.specialProbabilityProgressBar = null;
+	$scope.specialProbabilityResult = 0;
+	$scope.specialProbabilityColor = null;
+	$scope.isSpecialReady = true;
+	// calculate Special Up
+	$scope.calculateSpecialUp = function() {
+		if ($scope.copies == null || $scope.skillups == null) {
+			window.alert("Please enter your amount of copies and the amount of skillups needed");
+			return;
+		}
+		$scope.isSpecialReady = false;
+		var data = {copies: $scope.copies, skillups: $scope.skillups, special_event: $scope.specialEvent};
+		SpecialProbability.compute(data).then(
+			function(data) {
+				if (data.finished == true) {
+					if (data.result >= 0.6) {
+						$scope.specialProbabilityColor = "green";
+					} else if (data.result >= 0.3) {
+						$scope.specialProbabilityColor = "orange";
+					} else {
+						$scope.specialProbabilityColor = "red";
+					}
+					$scope.specialProbabilityResult = (data.result * 100).toFixed(1);
+					$scope.specialProbabilityProgressBar = "Complete";
+					$scope.isSpecialReady = true;
+				} 
+			}, 
+			null, 
+			function(data) {
+				$scope.specialProbabilityProgress = data.progress;
+				$scope.specialProbabilityProgressBar = data.progress + "%";
+			}
+		);
+	};
+	
+	$scope.socketProbabilityProgress = 0;
+	$scope.socketProbabilityProgressBar = null;
+	$scope.socketProbabilityResult = 0;
+	$scope.socketProbabilityColor = null;
+	$scope.isSocketReady = true;
+	// calculate Socket Up
+	$scope.calculateSocketUp = function() {
+		if ($scope.copies == null || $scope.character == null) {
+			window.alert("Please enter your amount of copies and pick the character you want to socket");
+			return;
+		}
+		$scope.isSocketReady = false;
+		// create slot array
+		var slots = Array.apply(null, Array($scope.slots)).map(function (x) { return 0; });;
+		for (i = 0; i < $scope.character.slots.length; ++i) {
+			console.log($scope.character.slots[i]);
+			var obj = $scope.character.slots[i];
+			if (obj != null) {
+				slots[i] = obj.level;
+			} else {
+				slots[i] = 0;
+			}
+		}
+		var data = {copies: $scope.copies, slots: slots};
+		SocketProbability.compute(data).then(
+			function(data) {
+				if (data.finished == true) {
+					if (data.result >= 0.6) {
+						$scope.socketProbabilityColor = "green";
+					} else if (data.result >= 0.3) {
+						$scope.socketProbabilityColor = "orange";
+					} else {
+						$scope.socketProbabilityColor = "red";
+					}
+					$scope.socketProbabilityResult = (data.result * 100).toFixed(1);
+					$scope.socketProbabilityProgressBar = "Complete";
+					$scope.isSocketReady = true;
+				} 
+			}, 
+			null, 
+			function(data) {
+				$scope.socketProbabilityProgress = data.progress;
+				$scope.socketProbabilityProgressBar = data.progress + "%";
+			}
+		);
+	};
+	
+    $rootScope.changeUnit = function(unit, uid) {
+        $scope.character = { uid: uid, slots: [ ] };
+		$scope.slots = $scope.slotCount(uid);
+    };
+
+    $scope.range = function(min, max) {
+        var result = new Array(max - min);
+        for (var i=0;i<result.length;++i) result[i] = min + i;
+        return result;
+    };
+
+    $scope.slotCount = function(uid) {
+        if (!uid) return 0;
+        return units[uid - 1].slots;
+    };
+
+    $scope.onRemove = function(i) {
+        $rootScope.character = null;
+        if (!$rootScope.$$phase) $rootScope.$apply();
+    };
+
+    $scope.generateURL = function() {
+
+        var result, character = $rootScope.character;
+
+        // team
+        var tokens = [ ];
+		var unit = character;
+		if (!unit || !unit.uid) tokens.push('!');
+		else {
+			var temp = unit.slots
+				.filter(function(x) { return x; })
+				.map(function(x) { return x.id + '' + x.level; })
+				.join('');
+			tokens.push(unit.uid + ':' + temp);
+		}
+        
+
+        result = '#/transfer/S' + tokens.join(',') + 'P';
+        $scope.url = window.location.href.match(/^(.+?)#/)[1] + result;
+
+        $timeout(function() {
+            $('#urlContainer > input').select();
+        });
+
+    };
+
+    var notifications = { };
+
+    $rootScope.notify = function(data) {
+        data = jQuery.extend({ type: 'information' },data);
+        if (data.name && notifications[data[name]]) notifications[data[name]].close(); 
+        var notification = noty(jQuery.extend({ timeout: 2500, layout: 'topRight', theme: 'relax' }, data));
+        if (data.name) notifications[data[name]] = notification;
+        return notification;
+
+    };
+    
+	$controller('StorageCtrl', { $scope: $scope });
+    $controller('DismissalCtrl');
+
+}];
+
+/**************
+ * PickerCtrl *
+ **************/
+
+controllers.PickerCtrl = function($scope, $state, $stateParams, $storage) { 
+
+    /* * * * * Scope variables * * * * */
+
+    $scope.units = [ ];
+    $scope.query = '';
+	$scope.recents = $storage.get('slotRecentUnits', [ ]);
+
+    $scope.isMats = $stateParams.mats;
+
+    $scope.$watch('query',function() { populateList(); },true);
+
+    /* * * * * Scope functions * * * * */
+
+    $scope.pickUnit = function(unitNumber) {
+        $scope.changeUnit($stateParams.unit, unitNumber + 1);
+		updateRecent(unitNumber);
+        $state.go('^');
+    };
+
+    /* * * * * List generation * * * * */
+
+    var populateList = function() {
+        $scope.units = [ ];
+        var result, parameters = Utils.generateSearchParameters($scope.query);
+        if (parameters === null) return;
+        result = window.units.filter(function(x) { return x !== null && x !== undefined && x.hasOwnProperty('number'); });
+        // filter by query
+        if (parameters.query) {
+            result = result.filter(function(unit) {
+                return parameters.query.test(Utils.getFullUnitName(unit.number + 1));
+            });
+        }
+        $scope.units = result;
+    };
+
+	var updateRecent = function(unitNumber) {
+        var recentUnits = JSON.parse(JSON.stringify($scope.recents));
+        var n = recentUnits.indexOf(unitNumber);
+        if (n < 0) recentUnits.unshift(unitNumber);
+        else recentUnits = recentUnits.splice(n,1).concat(recentUnits);
+        recentUnits = recentUnits.slice(0,16);
+		$storage.set('slotRecentUnits', recentUnits);
+    };
+
+};
+
+/***************
+ * StorageCtrl *
+ ***************/
+
+controllers.StorageCtrl = function($scope, $rootScope, $storage) {
+    var character = $storage.get('slotCharacter', null);
+    if (character === null) {
+		character = $rootScope.character;
+	} else {
+		$rootScope.character = character;
+	}
+};
+
+
+/*************
+ * ResetCtrl *
+ *************/
+
+controllers.ResetCtrl = function($scope, $rootScope, $state) {
+    $scope.resetStorage = function() {
+        $rootScope.character = null;
+        $state.go('^');
+    };
+};
+
+/******************
+ * Initialization *
+ ******************/
+
+for (var controller in controllers)
+    angular.module('optc')
+        .controller(controller, controllers[controller]);
+
+})();
---
+++ probability/js/socketprobability.js
@@ -0,0 +1,85 @@
+onmessage = function(e) {
+
+	var copies = parseInt(e.data.copies);
+	var slots = e.data.slots;
+	var temp = slots.slice(0);
+
+	console.log(slots);
+	
+	var approximationLimit = 500000;
+	var domain = 10;
+	var max_sockets = 5;
+	 //Array.apply(null, Array(slots)).map(function (x) { return 0; }); // intialize [0, ... , 0] length = #slots
+	var successful = 0;
+	var draw_result = 0;
+	var maxed = true;
+	
+	var socketProgress = 0;
+	var data = null;
+	
+	for (var i = 0; i < approximationLimit; i++) {
+		for (var j = 0; j < copies; j++) {
+			do {
+				draw_result = draw(0, domain - 1);
+				
+				// if socket is a socket we don't need
+				if (draw_result >= slots.length) {
+					break;
+				}
+				
+				// check if pulled socket is already maxed
+				if (slots[draw_result] < max_sockets) {
+					slots[draw_result]++;
+					break;
+				} 
+				
+				// we get here if socket pulled is already maxed and we pull again without subtracting the copy count (lazy solution)
+			} while (true);
+			
+		}
+		// check if all slots were maxed
+		for (var k = 0; k < slots.length; k++) {
+			if (slots[k] < max_sockets) {
+				maxed = false;
+				break;
+			}
+		}
+		
+		if (maxed == true) {
+			successful++;
+		}
+		// reset all variables
+		slots = temp.slice(0);
+		maxed = true;
+		drawResult = 0;
+		
+		if (i % 5000 == 0) {
+			//console.log(i);
+			
+			socketProgress = Math.round(i / approximationLimit * 100);
+			data = 
+			{
+				finished: false,
+				progress: socketProgress
+				
+			};
+			postMessage(data);
+	
+		}
+	}
+	//console.log("successful: " + successful);
+	//console.log(approximationLimit);
+	var prob = successful / approximationLimit;
+	data = 
+	{
+		finished: true,
+		result: prob,
+		progress: 100
+	};
+	postMessage(data);
+	
+}
+
+function draw(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
