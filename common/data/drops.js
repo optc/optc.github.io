667e8e2046291031d17f2eea76a7ea00fcd48cdc
optc-db <optc.db@gmail.com>
2016-09-20 21:57:35 +0200

    Drop icon

--- common/data/drops.js
+++ common/data/drops.js
@@ -1150,7 +1150,7 @@ window.drops = {
         
         {
             name: "Gather! Whitebeard Alliance",
-            thumb: null,
+            thumb: 1254,
             condition: "Driven Characters have their cooldowns reduced by 10 turns at the start of the fight",
             "All Difficulties": [ 1254, 1255, 1256, -669, -1077, -1079, -967, -972, -974, -750, -701, -500 ]
         },
