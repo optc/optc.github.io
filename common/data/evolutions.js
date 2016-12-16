bac411f26a66413f6ecbfc589fba53ee9b3ae39a
Zach <Zee_n1@yahoo.com>
2016-12-15 19:48:47 -0600

    Remove evolution back
    Creates infinite loop, crashes when launching new window.

--- common/data/evolutions.js
+++ common/data/evolutions.js
@@ -2111,16 +2111,8 @@ window.evolutions = {
         evolution: 1388,
         evolvers: [  ]
     },
-    1388: {
-        evolution: 8,
-        evolvers: [ 116, 117 ]
-    },
     12: {
         evolution: 1389,
         evolvers: [  ]
-    },
-    1389: {
-        evolution: 12,
-        evolvers: [ 116, 117 ]
     },
 };
