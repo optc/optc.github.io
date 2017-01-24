cb8cd5282548203d64ae9b2ba751a70222a0002f
ribcrush <marc.karam10@gmail.com>
2017-01-23 20:03:59 -0800

    Added information for don sai FN chars (incomplete)

--- common/data/details.js
+++ common/data/details.js
@@ -7674,29 +7674,35 @@ window.details = {
         special: "Deals 500 fixed damage to all enemies and reduces Special Cooldown of Striker characters by 1 turn.",
         specialName: "General Commander's Fight",
     },
-    1425: {//Don Sai Fortnight Character #1
-        captain: "",
-        special: "",
+    1425: {//Don Sai 3*
+        captain: "Boosts ATK of Fighter and Striker characters by 1.75x",
+        special: "Deals 25x character's ATK in QCK damage to one enemy.  When this special ativates when a character is locked, then for 3 turns boosts the effect of orbs by 1.5x",
+        specialName: "",
     },
-    1426: {//Don Sai Fortnight Character #1, Evolved
-        captain: "",
-        special: "",
+    1426: {//Don Sai 4*
+        captain: "Boosts ATK of Fighter and Striker characters by 2x and their HP by 1.25x",
+        special: "Deals 25x character's ATK in QCK damage to one enemy.  When this special ativates when a character is locked, then for 3 turns boosts the effect of orbs by 1.5x",
+        specialName: "",
     },
-    1427: {//Don Sai Fortnight Character #2
-        captain: "",
-        special: "",
+    1427: {//Boo 4*
+        captain: "Boosts ATK of QCK characters by 2x",
+        special: "Ignores defensive barriers and deals 15x character's ATK in QCK damage to one enemy",
+        specialName: "",
     },
-    1428: {//Don Sai Fortnight Character #3
-        captain: "",
-        special: "",
+    1428: {//Jean Ango 4*
+        captain: "Boosts amount of beli received by 1.5x, and gives chance of duplicating a drop upon completion of the island",
+        special: "When the enemy is delayed, reduces the special cooldown of all characters by 1 turn and boosts own attack by 1.75x for 1 turn",
+        specialName: "",
     },
     1429: {//Raid Franky
         captain: "",
         special: "",
+        specialName: "",
     },
     1430: {//Raid Franky, Evolved
         captain: "",
         special: "",
+        specialName: "",
     },
 };
 
