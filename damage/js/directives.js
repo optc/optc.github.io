a17a5af1f19fbd3d83d2831cf717a8f68144f254
pesaher <pesaher@gmail.com>
2017-02-04 21:02:44 +0100

    Update directives.js

--- damage/js/directives.js
+++ damage/js/directives.js
@@ -592,6 +592,7 @@ directives.unitOrb = function($rootScope) {
                 if (!$(e.target).hasClass('unitPortrait')) return;
                 if (unit.unit === null || /unitLevel/.test(e.target.className) || e.altKey || e.shiftKey) return;
 		$rootScope.notify({ text: !$rootScope.areGOrbsEnabled() && $rootScope.areSTROrbsEnabled() && n == ORBS.length - 3 ? "2" : "1" });
+		$rootScope.notify({ text: $rootScope.areGOrbsEnabled() ? ($rootScope.areSTROrbsEnabled() ? ORBS.length : ORBS.length - 1) : ($rootScope.areSTROrbsEnabled() ? ORBS.length : ORBS.length - 2)});
                 if (e.which == 2 || (e.which == 1 && (e.ctrlKey || e.metaKey || Utils.isClickOnOrb(e,e.target.parentNode)))) {
                     var n = ORBS.indexOf(tunit.orb);
                     tunit.orb = ORBS[(n + (!$rootScope.areGOrbsEnabled() && $rootScope.areSTROrbsEnabled() && n == ORBS.length - 3) ? 2 : 1) % ($rootScope.areGOrbsEnabled() ? ($rootScope.areSTROrbsEnabled() ? ORBS.length : ORBS.length - 1) : ($rootScope.areSTROrbsEnabled() ? ORBS.length : ORBS.length - 2))];
