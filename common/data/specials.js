2cdc31fb26a6ff001d6908630cf1c21b22d2199c
pesaher <pesaher@gmail.com>
2017-02-05 14:56:29 +0100

    Update specials.js

--- common/data/specials.js
+++ common/data/specials.js
@@ -1565,15 +1565,15 @@ window.specials = {
     1222: {
         turnedOn: false,
         onActivation: function(p) {
-            window.specials[1221].turnedOn = true;
+            window.specials[1222].turnedOn = true;
             if($scope.options.gOrbsEnabled > 1)
                 p.scope.notify({
                     text: 'While this special is active, [STR] orbs will have no effect. Please use matching orbs as [STR] orbs',
-                    name: '1221warning'
+                    name: '1222warning'
                 });
         },
         onDeactivation: function(p) {
-            window.specials[1221].turnedOn = false;
+            window.specials[1222].turnedOn = false;
         }
     },
     1227: {
