fc0532cb21f725427c77c0ec8d783b74aabe1d51
pesaher <pesaher@gmail.com>
2017-06-07 17:27:50 +0200

    Update directives.js

--- slots/js/directives.js
+++ slots/js/directives.js
@@ -64,7 +64,8 @@ directives.decorateSlot = function() {
                 if (!uid) element[0].style.backgroundImage = null;
                 else {
                     var url = Utils.getThumbnailUrl(scope.uid);
-                    element[0].style.backgroundImage = 'url(' + url + ')';
+                    var url2 = Utils.getGlobalThumbnailUrl(scope.uid);
+                    element[0].style.backgroundImage = 'url(' + url2 + '), url(' + url + ')';
                 }
             };
             scope.$watch('uid', update);
