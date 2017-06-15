db22952909fa7ece6fa094c93b55e0099674195f
JNathwani <jaylen.nathwani@gmail.com>
2017-06-15 15:42:14 -0400

    Stuff

--- common/js/nonSuspicious.js
+++ common/js/nonSuspicious.js
@@ -45,7 +45,7 @@ window.onload = function() {
     
     //Add new Units with Pics here so they show up in the Updates
     //7 Units are about the Max you can add, otherwise you would need to implement another tr for them
-    var newUnits = [ 1434, 1445, 1446, 1447, 1448, 1432, 1422 ];
+    var newUnits = [ 1619, 1610, 1621, 1623, 1624, 1606, 1605 ];
 
     for (var i = 0, len = newUnits.length; i < len; i++) {
         var unit = document.createElement("a");
