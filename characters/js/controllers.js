f365f3b85eef0384b1ca4de07d98e69f164007de
JNathwani <jaylen.nathwani@gmail.com>
2017-06-29 11:52:02 -0400

    Fixed Random?

--- characters/js/controllers.js
+++ characters/js/controllers.js
@@ -36,8 +36,8 @@ app.controller('MainCtrl',function($scope, $rootScope, $state, $stateParams, $ti
     $controller('DismissalCtrl');
     
     $scope.getRandChar = function(){
-        var range = $rootScope.table.data[$rootScope.table.data.length - 1][11] + 1;
-        return Math.floor(Math.random() * range);
+        var range = parseInt($rootScope.table.data.length) + 1;
+        return $rootScope.table.data[Math.floor(Math.random() * range)][0];
     };
 
 });
