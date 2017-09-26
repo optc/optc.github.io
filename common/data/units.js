b694052defb21e179fec22e472a934ede99bbe98
JNathwani <JNathwani@users.noreply.github.com>
2017-09-26 10:40:36 -0400

    Sanji sockets

--- common/data/units.js
+++ common/data/units.js
@@ -1813,7 +1813,7 @@ window.units = [
     [ "", "Type", [ "Class1", "Class2" ], null, null, null, null, null, null, null, null, null, null, null, null, null ],
     [ "Koala, Revolutionary Army's Secret Task Force", "PSY", [ "Fighter", "Free Spirit" ], 4, 15, 4, 4, 99, null, 238, 82, 18, 2422, 1012, 249, null ],
     [ "Vinsmoke Sanji", "STR", [ "Powerhouse", "Fighter" ], 4, 30, null, null, 70, 1066998, null, null, null, null, null, null, null ],
-    [ "Vinsmoke Sanji, Evolved", "STR", [ "Powerhouse", "Fighter" ], 5, 50, 4, null, 99, 5000000, 1, 1, 1, 2267, 1468, 346, null ],
+    [ "Vinsmoke Sanji, Evolved", "STR", [ "Powerhouse", "Fighter" ], 5, 50, 4, 3, 99, 5000000, 1, 1, 1, 2267, 1468, 346, null ],
     [ "Portgas D. Ace, The Sun that will Destroy the Darkness", "DEX", [ "Shooter", "Free Spirit" ], '6+', 65, 4, 5, 99, 5000000, 1, 1, 1, 3922, 1466, 358, null ],
     ];
     
