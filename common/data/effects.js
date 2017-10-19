f0a0de20da69193bec9b802868ae5c342c230f94
Jan Raventós <janraven55@gmail.com>
2017-10-19 06:00:06 +0200

    Neo Akainu Boosted Units
    Was requested on Marineford Discord

--- common/data/effects.js
+++ common/data/effects.js
@@ -426,5 +426,29 @@ window.effects = {
             return !matching ? 1 : 2.5;
         },
 	},
+	
+	'Akainu Neo - Slasher Raid Boost': {
+        description: 'Certain units boosted',
+        thumb: 1826,
+        id: 44, // don't change this
+        atk: function(p) {
+            var id = p.number + 1,
+                matching = ((id > 1742 && id < 1745) ||
+                    [ 1795, 1796, 1539, 1540, 1823, 1844, 1277, 1278 ].indexOf(id) != -1);
+            return !matching ? 1 : 1.75;
+        },
+        hp: function(p) {
+            var id = p.number + 1,
+                matching = ((id > 1742 && id < 1745) ||
+                    [ 1795, 1796, 1539, 1540, 1823, 1844, 1277, 1278 ].indexOf(id) != -1);
+            return !matching ? 1 : 1.75;
+        },
+        rcv: function(p) {
+            var id = p.number + 1,
+                matching = ((id > 1742 && id < 1745) ||
+                    [ 1795, 1796, 1539, 1540, 1823, 1844, 1277, 1278 ].indexOf(id) != -1);
+            return !matching ? 1 : 1.75;
+        },
+	},
     
 };
