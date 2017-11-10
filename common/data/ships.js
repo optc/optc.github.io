03c95170babebd6e58d809a1bfa3cd28801348ad
JNathwani <JNathwani@users.noreply.github.com>
2017-11-10 14:49:26 -0500

    Ship image

--- common/data/ships.js
+++ common/data/ships.js
@@ -431,7 +431,7 @@ window.ships = [
     
     { //33
         name: "Revolutionary Black Crow",
-        thumb: 'ship_0034_c.png',
+        thumb: 'ship_0035_c.png',
         description: 'Boosts HP by 1.2x and reduces special cooldown of all characters by 1 at the start of the adventure. Boosts ATK of [STR], [DEX] and [QCK] characters by 1.55x if you have a [STR], [DEX] and [QCK] character on your crew.',
         atk: function(p) { 
             return (p.colorCount.STR>=1 && p.colorCount.DEX>=1 && p.colorCount.QCK>=1) ? [ 1.2, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.5, 1.55 ][p.boatLevel - 1] : 1;
