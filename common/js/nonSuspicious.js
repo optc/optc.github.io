bbd8c9066e1318e4da9e6379e97c6838c5798845
TheLucky007 <justateste111@gmail.com>
2018-05-18 14:13:29 -0400

    Bandai update

--- common/js/nonSuspicious.js
+++ common/js/nonSuspicious.js
@@ -45,7 +45,7 @@ window.onload = function() {
     
     //Add new Units with Pics here so they show up in the Updates
     //7 Units are about the Max you can add, otherwise you would need to implement another tr for them
-    var newUnits = [ 2070, 2068, 2066, 2064 ];
+    var newUnits = [ 2091, 2082, 2080, 2078, 2076, 2074, 2072 ];
 
     for (var i = 0, len = newUnits.length; i < len; i++) {
         var unit = document.createElement("a");
