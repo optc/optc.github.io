f3272d40b34587c89fdcc9c3bc0215c14509153c
JNathwani <jaylen.nathwani@gmail.com>
2018-10-08 11:19:32 -0400

    Brulee classes

--- common/data/units.js
+++ common/data/units.js
@@ -2112,7 +2112,7 @@ window.units = [
     [ "Eustass Kid, The Worst Generation Aiming at \"Red Hair\"", "DEX", [ "Striker", "Shooter" ], 5, 40, 4, 4, 99, 4000000, 1522, 863, 105, 2849, 1395, 237, 1 ],
     [ "Charlotte Katakuri", "STR", [ "Shooter", "Cerebral" ], 5, 30, 5, 3, 70, 1066998, 595, 330, 50, 1750, 975, 150, 1 ],
     [ "Charlotte Katakuri, Big Mom Pirates 3 Sweet Commanders", "STR", [ "Shooter", "Cerebral" ], 6, 55, 4, 5, 99, 5000000, 1675, 910, 100, 3700, 1640, 250, 1 ],
-    [ "Charlotte Brûlée", "PSY", [ "Cerebral", "Striker" ], 4, 15, 6, 3, 50, 392109, 99, 150, 59, 891, 783, 214, 1 ],
+    [ "Charlotte Brûlée", "PSY", [ "Cerebral", "Fighter" ], 4, 15, 6, 3, 50, 392109, 99, 150, 59, 891, 783, 214, 1 ],
     [ "Charlotte Brûlée, Big Mom Pirates", "PSY", [ "Cerebral", "Striker" ], 5, 30, 4, 4, 99, 4000000, 810, 720, 149, 1570, 1401, 402, 1 ],
     [ "Charlotte Amande", "INT", [ "Slasher", "Driven" ], 4, 15, 6, 3, 50, 392109, 135, 194, -100, 989, 881, -100, 1 ],
     [ "Charlotte Amande, Big Mom Pirates", "INT", [ "Slasher", "Driven" ], 5, 30, 6, 4, 99, 4000000, 910, 820, -100, 2250, 1500, -100, 1 ],
