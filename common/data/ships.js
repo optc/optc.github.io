3a62d789464836a35d5ea78330cd3b1978294ac0
Solaris <iSolaris@Jaylens-MacBook-Pro.local>
2018-10-17 00:30:53 -0400

    Wording

--- common/data/ships.js
+++ common/data/ships.js
@@ -147,9 +147,9 @@ window.ships = [
     },
 
     { // 11
-        name: 'Ace\'s Striker',
+        name: 'Striker',
         thumb: 'ship_0012_c1.png',
-        description: 'Boosts ATK of Shooter characters by 1.5x and their HP by 1.3x, reduces cooldown of all specials by 1 turn at the start of the fight',
+        description: 'Boosts ATK of Shooter characters by 1.5x and their HP by 1.3x, reduces cooldown of Shooter specials by 1 turn at the start of the fight',
         atk: function(p) {
             return !p.unit.class.has('Shooter') ? 1 :
                 [ 1.2, 1.2, 1.2, 1.25, 1.25, 1.3, 1.3, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
