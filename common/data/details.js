a856c65c0ef7d6d86f0a06e8aabb4ed1d94e4a60
JNathwani <JNathwani@users.noreply.github.com>
2019-12-08 17:15:37 -0500

    typo

--- common/data/details.js
+++ common/data/details.js
@@ -53904,7 +53904,7 @@ window.details = {
             { "description": "Boosts base HP by 60" },
             { "description": "Reduce base Special Cooldown by 1 turn" },
             { "description": "Acquire 1 additional Socket slot" },
-            { "description": "Acquire Potential 2: Reduce No Healing duration" },
+            { "description": "Acquire Potential 2: Critical Hit" },
         ],
         potential: [
             {
