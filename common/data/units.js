1c74365bf498212c178f935075f31f890c7c52ad
JNathwani <JNathwani@users.noreply.github.com>
2020-02-19 16:55:16 -0500

    mom combo

--- common/data/units.js
+++ common/data/units.js
@@ -2470,7 +2470,7 @@ window.units = [
     [ "Smoothie & Oven, The Duo that will Eliminate the Intruders", [ "PSY", "QCK" ], [[ "Powerhouse", "Slasher" ], [ "Powerhouse", "Fighter" ], [ "Powerhouse", "Slasher" ]], 5, 60, 6, 1, 99, 5000000, 1420, 780, 212, 2672, 1428, 322, 1 ],
     [ "Charlotte Katakuri, Charlotte Family's Second Son", "INT", [ "Powerhouse", "Striker" ], 4, 25, 4, 3, 70, 1066998, 804, 481, 40, 1512, 830, 132, 1 ],
     [ "Charlotte Katakuri, Mochi-Mochi Fruit User", "INT", [ "Powerhouse", "Striker" ], 5, 40, 6, 4, 99, 4000000, 1460, 780, 120, 2871, 1403, 258, 1 ],
-    [ "Big Mom, Hungry Pursuit", "QCK", [ "Slasher", "Powerhouse" ], 4, 30, 2, 5, 70, 1066998, 458, 245, 35, 1944, 731, 110, 1 ],
+    [ "Big Mom, Hungry Pursuit", "QCK", [ "Slasher", "Powerhouse" ], 4, 30, 5, 2, 70, 1066998, 458, 245, 35, 1944, 731, 110, 1 ],
     [ "Big Mom, Hungry Onslaught", "QCK", [ "Slasher", "Powerhouse" ], 5, 50, 5, 3, 99, 5000000, 1466, 632, 78, 3001, 1450, 200, 1 ],
     [ "Dracule Mihawk, World's Strongest Teacher", "DEX", [ "Slasher", "Free Spirit" ], 5, 30, 4, 3, 70, 1066998, 840, 234, 44, 2011, 601, 109, 1 ],
     [ "Dracule Mihawk, Teaching the Secret Techniques of a Great Swordsman", "DEX", [ "Slasher", "Free Spirit" ], 6, 55, 4, 5, 99, 5000000, 1731, 576, 98, 3854, 1621, 221, 1 ],
