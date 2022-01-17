124390cc3721c1a52a03d91ed8754a66be5a82bd
Solaris <12967322+Solaris333@users.noreply.github.com>
2022-01-16 20:17:59 -0500

    Revert "Revert "Merge pull request #356 from CodeYan01/twoSpecials""
    This reverts commit 9af9de4443226a3962448ca3fd8f548f2c7de3d5.

--- common/js/unitUtils.js
+++ common/js/unitUtils.js
@@ -6,6 +6,7 @@ var UnitUtils = { };
 
 UnitUtils.extendDouble = function(state, baseid, type) {
     var types = [ "character1", "character2" ];
+    var newId = state["start"]+state["increment"];
     if(type == "dual"){
         if (Array.isArray(window.units[baseid - 1][1])){
             if(window.units[baseid - 1][1][0] == window.units[baseid - 1][1][1]){
@@ -16,16 +17,22 @@ UnitUtils.extendDouble = function(state, baseid, type) {
     }
     //if (type == "vs") console.log(baseid, types, window.units[baseid - 1][1], Array.isArray(window.units[baseid - 1][1]));
     for(var i in types) {
-        window.details[state["start"]+state["increment"]] = Object.assign({},window.details[baseid]);
+        newId = state["start"]+state["increment"];
+        window.details[newId] = Object.assign({},window.details[baseid]);
         if (type == "dual"){
-            window.details[state["start"]+state["increment"]]["captain"] = window.details[baseid]["captain"][types[i]];
-            if (window.details[baseid].sailor) window.details[state["start"]+state["increment"]]["sailor"] = { "base": window.details[baseid]["sailor"][types[i]], "level1": window.details[baseid]["sailor"]["level1"], };
+            window.details[newId]["captain"] = window.details[baseid]["captain"][types[i]];
+            // dual characters with two specials
+            if (window.details[newId].special.character1) {
+                // either "character1" or "character2" only
+                window.details[newId].special = window.details[baseid].special[types[i % 2]];
+            }
+            if (window.details[baseid].sailor) window.details[newId]["sailor"] = { "base": window.details[baseid]["sailor"][types[i]], "level1": window.details[baseid]["sailor"]["level1"], };
         }
         if (type == "vs"){
-            window.details[state["start"]+state["increment"]]["captain"] = window.details[baseid]["captain"][types[i]];
-            window.details[state["start"]+state["increment"]]["special"] = window.details[baseid]["special"][types[i]];
-            window.details[state["start"]+state["increment"]]["VSSpecial"] = window.details[baseid]["VSSpecial"][types[i]];
-            if (window.details[baseid].sailor) window.details[state["start"]+state["increment"]]["sailor"] = { "base": window.details[baseid]["sailor"][types[i]], "level1": window.details[baseid]["sailor"]["level1"], };
+            window.details[newId]["captain"] = window.details[baseid]["captain"][types[i]];
+            window.details[newId]["special"] = window.details[baseid]["special"][types[i]];
+            window.details[newId]["VSSpecial"] = window.details[baseid]["VSSpecial"][types[i]];
+            if (window.details[baseid].sailor) window.details[newId]["sailor"] = { "base": window.details[baseid]["sailor"][types[i]], "level1": window.details[baseid]["sailor"]["level1"], };
         }
         state["increment"] += 1;
     }
